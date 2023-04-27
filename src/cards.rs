deb07dc4d3c20d3f53c036918b6389516f1b7346
Steven Carter <steven.carter@bounteous.com>
2023-04-27 19:19:01 -0400

    Adding display logic

--- src/cards.rs
+++ src/cards.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Card {
     pub color: Option<String>,
     pub count: u8,
@@ -5,29 +6,41 @@ pub struct Card {
 }
 
 pub fn init_cards() -> Vec<Card> {
-let colors: Vec<&str> = vec!["red", "yellow", "green", "blue", "purple", "orange"];
-let symbols: Vec<&str> = vec!["lollipop", "cone", "peppermint", "gumdrop", "fudge"];
+    let colors: Vec<&str> = vec!["red", "yellow", "green", "blue", "purple", "orange"];
+    let symbols: Vec<&str> = vec!["lollipop", "cone", "peppermint", "gumdrop", "fudge"];
 
     let mut cards: Vec<Card> = Vec::new();
 
     for color in colors {
         for _ in 0..4 {
-            cards.push(Card { color: Some(color.to_string()), count: 1, symbol: None });
+            cards.push(Card {
+                color: Some(color.to_string()),
+                count: 1,
+                symbol: None,
+            });
         }
         for _ in 0..3 {
-            cards.push(Card { color: Some(color.to_string()), count: 2, symbol: None });
+            cards.push(Card {
+                color: Some(color.to_string()),
+                count: 2,
+                symbol: None,
+            });
         }
     }
     for symbol in symbols {
-        cards.push(Card { color: None, count: 1, symbol: Some(symbol.to_string())});
+        cards.push(Card {
+            color: None,
+            count: 1,
+            symbol: Some(symbol.to_string()),
+        });
     }
 
-    shuffle(&mut cards);
+    shuffle_deck(&mut cards);
 
     cards
 }
 
-fn shuffle<T>(vector: &mut Vec<T>) {
+fn shuffle_deck<T>(vector: &mut Vec<T>) {
     // Get the length of the vector.
     let length = vector.len();
 
@@ -38,5 +51,26 @@ fn shuffle<T>(vector: &mut Vec<T>) {
 
         // Swap the current element with the element at the random index.
         vector.swap(i, random_index);
+    }
+}
+
+pub fn get_card(in_cards: Vec<Card>) -> (Card, Vec<Card>) {
+    let mut cards = in_cards;
+    if cards.is_empty() {
+        cards = init_cards();
+    }
+
+    (cards.pop().unwrap(), cards)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_builds_a_deck() {
+        let deck = init_cards();
+        assert_eq!(deck.len(), 47);
+        println!("{:#?}", deck);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,75 @@
 mod cards;
+use crate::cards::{get_card, init_cards, Card};
 use yew::prelude::*;
+
+fn card_value(card: Card) -> String {
+    format!("{:?} - {:?} - {:?}", card.color, card.count, card.symbol)
+}
+
+#[derive(Properties, PartialEq)]
+pub struct CardProps {
+    pub card: Card,
+}
+
+#[function_component]
+fn CardBlock(props: &CardProps) -> Html {
+    let card = props.card.clone();
+
+    if card.color.is_some() {
+        if card.count == 2 {
+            let color = card.color.unwrap();
+            html! {
+                <div class="card">
+                    <div>{color.clone()} {2}</div>
+                    <div class={classes!("colorbox", color.clone())} />
+                    <div class={classes!("colorbox", color.clone())} />
+                </div>
+            }
+        } else {
+            let color = card.color.unwrap();
+            html! {
+                <div class="card">
+                    <div>{color.clone()} {1}</div>
+                    <div class={classes!("colorbox", color.clone())} />
+                </div>
+            }
+        }
+    } else if card.symbol.is_some() {
+        html! {
+            <div class="card">
+                { card.symbol.unwrap()}
+            </div>
+        }
+    } else {
+        html! {
+            <div class="card">
+                { "get your first card" }
+            </div>
+        }
+    }
+}
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
+    let cards: UseStateHandle<Vec<Card>> = use_state(|| init_cards());
+    let card: UseStateHandle<Card> = use_state(|| Card {
+        color: None,
+        count: 0,
+        symbol: None,
+    });
+    let next_card = {
+        let card = card.clone();
+        let cards = cards.clone();
+        Callback::from(move |_| {
+            let (new_card, new_cards) = get_card((*cards).clone());
+            card.set(new_card);
+            cards.set(new_cards);
+        })
     };
 
     html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
+        <div class="container">
+            <button onclick={next_card}><CardBlock card={(*card).clone()} /></button>
         </div>
     }
 }
